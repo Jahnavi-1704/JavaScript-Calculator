bfd970218dda4649a59254388885dd51ab145d76
Jahnavi-1704 <59229521+Jahnavi-1704@users.noreply.github.com>
2020-10-16 17:46:16 +0300

    Add files via upload

---
+++ function.js
@@ -0,0 +1,11 @@
+function calcNumbers(result)
+    {
+	
+        form.displayResult.value=form.displayResult.value+result;
+        
+    }
+
+function clr()
+    {
+	form.displayResult.value="";
+    }
